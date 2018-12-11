ba70dd0b9b8718a7d645503b77404c28e2ead78c
Bronson Bradshaw <bronbrad@byu.edu>
2018-12-11 16:34:07 -0700

    changed models

---
+++ Project2/Models/Cases.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrapoDeeds.Models
+{
+    public class Cases
+    {
+        [Key]
+        public int caseID
+        {
+            get;
+            set;
+        }
+        [ForeignKey("Clients")]
+        [Display(Name = "Client ID")]
+        public int clientID
+        {
+            get;
+            set;
+        }
+        [Display(Name = "Case Name")]
+        public string caseName
+        {
+            get;
+            set;
+        }
+        [ForeignKey("Tasks")]
+        public int? taskID { get; set; }
+        public virtual ICollection<Tasks> tasks { get; set; }
+
+    }
+}
