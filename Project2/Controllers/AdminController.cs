b40d573931f24aab5f6e84946934cbffeaa69be7
Radley Nelson <ern23@byu.edu>
2018-12-12 17:03:23 -0700

    Added Admin Stuff

---
+++ Project2/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Project2.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project2.Controllers
+{
+    public class AdminController : Controller
+    {
+        private DefaultConnection db = new DefaultConnection();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            ViewBag.caseSize = db.Cases.Count();
+            ViewBag.taskSize = db.Tasks.Count();
+            ViewBag.clientSize = db.Clients.Count();
+
+            ViewBag.tasks = db.Tasks.ToList();
+           
+            return View();
+        }
+    }
+}
